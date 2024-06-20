a2f89ec8188d9e4ce34baa814d2076374c4af2f6
Nafis Ahamed <nafisahamed14@gmail.com>
2024-06-20 19:59:48 +0600

    error loading

---
+++ src/app/error.jsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react';
+
+const error = () => {
+    return (
+        <div className='text-blue-500'>
+            common error ....
+        </div>
+    );
+};
+
+export default error;
---
+++ src/app/loading.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const loading = () => {
+    return (
+        <div>
+            common loading.....
+        </div>
+    );
+};
+
+export default loading;
