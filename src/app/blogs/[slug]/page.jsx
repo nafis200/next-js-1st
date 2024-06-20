e34d58d2ca93b9589bf95f5e5d87a9c8a937b998
Nafis Ahamed <nafisahamed14@gmail.com>
2024-06-20 18:24:17 +0600

    dynamic routinting

--- src/app/blogs/[slug]/page.jsx
+++ src/app/blogs/[slug]/page.jsx
@@ -2,10 +2,14 @@
 
 
 
-const page = () => {
+const page = ({params}) => {
+    // console.log(params)
+    console.log(params.slug)
+    
     return (
         <div>
             <h2 className="text-2xl">Hellow how are you</h2>
+            <h2 className="text-2xl">{params.slug}</h2>
         </div>
     );
 };
