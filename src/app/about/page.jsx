9febc450319d43e978144f7262b26a2bf9d9c031
Nafis Ahamed <nafisahamed14@gmail.com>
2024-06-21 18:42:34 +0600

    miftahul

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,6 +3,7 @@ import Aboutroute from '@/components/Aboutroute';
 import Nafis from '@/components/Nafis';
 import React from 'react';
 import { Headland_One } from 'next/font/google';
+// import { redirect } from 'next/navigation';
 
 const headlan =  Headland_One({weight:['400'],subsets:['latin']})
 
@@ -11,11 +12,24 @@ export const metadata = {
     description: "miles",
   };
 
-const Aboutpage = () => {
+  const getTime = async()=>{
+    const res = await fetch(`http://localhost:3000/time`,{cache:'no-store'})
+    const data = await res.json()
+    // if(data){
+    //    redirect('/meals')
+    // }
+    return data.currentTime
+
+    // const res = await fetch(`http://localhost:3000/time`,{next:{revalidate:5}})
+  }
+
+const Aboutpage = async() => {
+    const currentTime = await getTime()
     return (
         <div className={`${headlan.className}`}>
              <Aboutroute></Aboutroute>
             <Nafis></Nafis>
+            <h2 className='text-center p-6'>{currentTime}</h2>
         </div>
     );
 };
--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import React from 'react';
 
 const getPosts = async()=>{
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/posts`)
     const data = await res.json() 
     return data
 }
