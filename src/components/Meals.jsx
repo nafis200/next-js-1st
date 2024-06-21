8680bd0c981355ef09678bbc7cc16bfdb9298477
Nafis Ahamed <nafisahamed14@gmail.com>
2024-06-21 11:48:54 +0600

    meal search

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 
 const Meals = () => {
     const [search,setSearch] = useState("")
-    const [meals,setMeals] = useState("")
+    const [meals,setMeals] = useState([])
 
     const leadData = async()=>{
         const res = await fetch(`https://jsonplaceholder.typicode.com/comments?email=${search}`)
@@ -24,6 +24,17 @@ const Meals = () => {
             <div className="mt-10">
                 <input onChange={handler} className="p-4 outline-none border-transparent text-slate-700" type="text" placeholder="Search meals" />
                 <button onClick={leadData} className="btn bg-red-400 p-4">Search</button>
+            </div>
+            <div>
+                {
+                    meals?.map((meal)=>(
+                        <div key={meal.id}>
+                            <h1>{meal.postId}</h1>
+                            <h1>{meal.email}</h1>
+                            <h1>{meal.name}</h1>
+                        </div>
+                    ))
+                }
             </div>
         </div>
     );
