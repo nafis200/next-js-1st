406f85f05521b9439601a11cbb9ab7aabfed3d15
Nafis Ahamed <nafisahamed14@gmail.com>
2024-06-22 17:51:59 +0600

    form

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,12 +1,24 @@
 
 import NextAuth from "next-auth/next"
-
+import CredentialsProvider from "next-auth/providers/credentials";
 const handler = NextAuth({
     session:{
         strategy: 'jwt'
     },
     providers:[
-
+        CredentialsProvider({
+            credentials: {
+              email: { label: "email", type: "text",  required:true, placeholder: "your email"},
+              password: { label: "Password", type: "password", required:true, placeholder: "your password" }
+            },
+            async authorize(credentials) {
+              if (!credentials) {
+                return null
+              } else {
+                return true
+              }
+            }
+        })
     ],
 })
 
